fe32c4956a60ff8d94b4b859db448c518ef56105
Naushin <naushin.r@gmail.com>
2023-09-12 23:47:21 -0400

    added moon phase of previous & following 2 days

--- index.js
+++ index.js
@@ -31,8 +31,9 @@
 //     console.error('Error:', error);
 //   });
 
+let currentDate = new Date();
 
-var postData = {
+var origData = {
   "format": "png",
   "style": {
     "moonStyle": "default",
@@ -42,14 +43,33 @@ var postData = {
       "textColor": "white"
     },
     "observer": {
-      "latitude": 6.56774,
-      "longitude": 79.88956,
-      "date": "2020-09-11"
+      "latitude": 43.65348,
+      "longitude": -79.3839347,
+      "date": currentDate
   },
   "view": {
-    "type": "landscape-simple",
+    "type": "portrait-simple",
     "orientation": "north-up"
   }
+}
+
+function getLatLon() {
+  const locationInput = document.getElementById('city').value;
+  const APIKey = "e4875d42d4bb143769832d3c52da81bf";
+  var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=1&appid=${APIKey}`;
+
+  async function getCoords() {
+    try {
+      const response = await axios.get(apiUrl);
+      console.log(response);
+      origData.observer.latitude = response.data[0].lat.toFixed(5);
+      origData.observer.longitude = response.data[0].lon.toFixed(5);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getCoords();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -60,29 +80,27 @@ document.addEventListener('DOMContentLoaded', function () {
 function handleSubmit(event) {
   
   const dateInput = document.getElementById('date').value;
-  const locationInput = document.getElementById('city').value;
-  
-  'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
   
   // Create a data object with the user input
-  postData.observer.date = dateInput;
-
+  origData.observer.date = dateInput;
+  getLatLon();
   event.preventDefault();
 
-  APIRequest(postData);
+  moonRequest(origData);
 }
 
-function APIRequest(data) {
-  const appID = "96b04335-8a59-44f3-80ba-5f79d6b5bdb2";
-  const appSecret = "5fa633cf6abeb4329aeec9ac79b07f66e9c5ea082fcaff09ded32495f9ff10512aba0886f81c10bf5fe079c49ebb84590d9661437f5abcac83f9c102085e0e842b53dfa2531b0c75a4df71c307adafdf51e5bf532628459576f05438b93d40a27c85e0aaf2a83d2f559afd0a9d312297";
-  const authString = btoa(`${appID}:${appSecret}`);
-  
-  const headers = new Headers({
-    'Authorization': `Basic ${authString}`,
-    'Content-Type': 'application/json',
-  });
-  
-  const apiUrl = 'https://api.astronomyapi.com/api/v2/studio/moon-phase';
+const appID = "96b04335-8a59-44f3-80ba-5f79d6b5bdb2";
+const appSecret = "5fa633cf6abeb4329aeec9ac79b07f66e9c5ea082fcaff09ded32495f9ff10512aba0886f81c10bf5fe079c49ebb84590d9661437f5abcac83f9c102085e0e842b53dfa2531b0c75a4df71c307adafdf51e5bf532628459576f05438b93d40a27c85e0aaf2a83d2f559afd0a9d312297";
+const authString = btoa(`${appID}:${appSecret}`);
+
+const apiUrl = 'https://api.astronomyapi.com/api/v2/studio/moon-phase';
+
+const headers = new Headers({
+  'Authorization': `Basic ${authString}`,
+  'Content-Type': 'application/json',
+});
+
+function moonRequest(data) {
   
   const fetchOptions = {
     method: 'POST', // HTTP method 
@@ -102,9 +120,40 @@ function APIRequest(data) {
     let moonpic = document.createElement("img");
     moonpic.src = data.data.imageUrl;
     var moonPhase = document.querySelector('#moon-phase');
+    moonpic.style.width = '15rem';
+    moonpic.style.height = '20rem';
     moonPhase.appendChild(moonpic);
     })
     .catch(error => {
       console.error('Error:', error);
-    });
+    });   
 }
+
+function defaultMoons(data) {
+  const today = new Date();
+
+  const previous2 = new Date(today);
+  previous2.setDate(previous2.getDate() - 2);
+  origData.observer.date = previous2;
+  moonRequest(data);
+
+  const previous1 = new Date(today);
+  previous1.setDate(previous1.getDate() - 1);
+  origData.observer.date = previous1;
+  moonRequest(data);
+
+  origData.observer.date = today;
+  moonRequest(data);
+
+  const next1 = new Date(today);
+  next1.setDate(next1.getDate() + 1);
+  origData.observer.date = next1;
+  moonRequest(data);
+
+  const next2 = new Date(today);
+  next2.setDate(next2.getDate() + 2);
+  origData.observer.date = next2;
+  moonRequest(data);
+}
+
+window.onload = defaultMoons(origData);
