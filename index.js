e36dc5e435379205925aee5418cca7b9d3eeaa89
Naushin Rahman <naushin.r@gmail.com>
2023-11-04 15:16:48 -0400

    adding express.js API

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ var starData =  {
     }
 }
 
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000; // You can choose any port you like
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files from a 'public' directory
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Assuming your HTML file is in the same directory as your JS file
+});
+
 document.addEventListener('DOMContentLoaded', async function () {
   
   const form = document.getElementById('moonForm');
